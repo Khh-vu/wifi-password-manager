b67f78a48ce21650f2d0b02379b533b0382b3556
Khh-vu <75486814+Khh-vu@users.noreply.github.com>
2026-04-29 13:00:45 +0700

    Bump app version to 1.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "io.github.wifi_password_manager"
         minSdk { version = release(30) }
         targetSdk { version = release(37) }
-        versionCode = 12
-        versionName = "1.11"
+        versionCode = 13
+        versionName = "1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
