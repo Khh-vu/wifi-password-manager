3489bf103a153605416fd64ab7e6d877812bc34c
Khh-vu <75486814+Khh-vu@users.noreply.github.com>
2026-04-29 11:38:57 +0700

    Add Japanese language support

--- app/src/main/java/io/github/wifi_password_manager/domain/model/Settings.kt
+++ app/src/main/java/io/github/wifi_password_manager/domain/model/Settings.kt
@@ -50,5 +50,6 @@ data class Settings(
         ENGLISH("en", "English"),
         RUSSIAN("ru", "Русский"),
         CHINESE_SIMPLIFIED("zh-CN", "中文 (简体)"),
+        JAPANESE("ja", "日本語"),
     }
 }
